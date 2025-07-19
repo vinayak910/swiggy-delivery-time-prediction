c0c2a249a75a12a518b44ffb280919cc2739081d
Vinayak <vinayakchhabra545.vc@gmail.com>
2025-07-19 22:43:20 +0530

    added model training stage

--- src/features/data_preprocessing.py
+++ src/features/data_preprocessing.py
@@ -1,27 +1,18 @@
-import pandas as pd 
-import numpy as np 
-import joblib 
+import pandas as pd
 import logging
-from pathlib import Path 
+from pathlib import Path
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, MinMaxScaler, OrdinalEncoder 
+from sklearn.preprocessing import (
+    OneHotEncoder, 
+    MinMaxScaler, 
+    OrdinalEncoder)
+import joblib
+from sklearn import set_config
 
+# set the transformer outputs to pandas
+set_config(transform_output='pandas')
 
-from sklearn import set_config 
-
-set_config(transform_output= "pandas")
-
-
-logger = logging.getLogger(name = "data_preprocessing")
-logger.setLevel(logging.INFO)
-
-handler = logging.StreamHandler()
-handler.setLevel(logging.INFO)
-
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-handler.setFormatter(formatter)
-
-logger.addHandler(handler)
+# columns to preprocess in data
 
 num_cols = ["age",
             "ratings",
@@ -38,111 +29,104 @@ nominal_cat_cols = ['weather',
 
 ordinal_cat_cols = ["traffic","distance_type"]
 
+target_col = "time_taken"
+
+# generate order for ordinal encoding
+
 traffic_order = ["low","medium","high","jam"]
 
 distance_type_order = ["short","medium","long","very_long"]
 
-target_col = 'time_taken'
+# create logger
+logger = logging.getLogger("data_preprocessing")
+logger.setLevel(logging.INFO)
 
-def load_data(data_path: Path)->pd.DataFrame:
-    """
-    Loads data from a CSV file.
+# console handler
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
 
-    Args:
-        data_path (Path): Path to the CSV file.
+# add handler to logger
+logger.addHandler(handler)
 
-    Returns:
-        pd.DataFrame or None: Loaded DataFrame or None if file not found.
-    """
+# create a fomratter
+formatter = logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# add formatter to handler
+handler.setFormatter(formatter)
+
+def load_data(data_path: Path) -> pd.DataFrame:
     try:
         df = pd.read_csv(data_path)
-        logger.info(f"Data loaded successfully from {data_path}")
-        return df
-    except FileNotFoundError:
-        logger.error(f"File not found at {data_path}")
-    except Exception as e:
-        logger.error(f"Error while loading data: {e}")
-    return None
-
-
-def drop_missing_values(df:pd.DataFrame)->pd.DataFrame:
-
-    logger.info(f"The original dataset with missing values has {df.shape} shape.")
-
-    df_dropped = df.dropna()
-
-    logger.info(f"The dataset with missing values drop has a shape of {df_dropped.shape}")
-
-    missing_values = df_dropped.isna().sum().sum()
-
-    if missing_values>0:
-        
-        raise ValueError("The dataset still has missing values")
     
+    except FileNotFoundError:
+        logger.error("The file to load does not exist")
+    
+    return df
+
+
+def drop_missing_values(data: pd.DataFrame) -> pd.DataFrame:
+
+    logger.info(f"The original dataset with missing values has {data.shape[0]} rows and {data.shape[1]} columns")
+    df_dropped = data.dropna()
+    logger.info(f"The dataset with missing values dropped has {df_dropped.shape[0]} rows and {df_dropped.shape[1]} columns")
+    missing_vals = df_dropped.isna().sum().sum()
+    
+    if missing_vals > 0:
+        raise ValueError("The dataframe has missing values")
     return df_dropped
 
 
-def make_X_and_y(data:pd.DataFrame , target_column:str):
-
-    X = data.drop(columns=target_column)
-    y = data[target_column]
-
-    return X , y
-
-
-def train_preprocessor(preprocessor , df: pd.DataFrame):
-    preprocessor.fit(df)
+def save_transformer(transformer, save_dir: Path, transformer_name: str):
+    # form the save location
+    save_location = save_dir / transformer_name
+    # save the transformer
+    joblib.dump(value=transformer,filename=save_location)
+    
+def train_preprocessor(preprocessor, data: pd.DataFrame):
+    # fit on the data
+    preprocessor.fit(data)
     return preprocessor
 
+def perform_transformations(preprocessor, data: pd.DataFrame):
+    # transform the data
+    transformed_data = preprocessor.transform(data)
+    return transformed_data
 
-def perform_transformations(preprocessor , df: pd.DataFrame):
+def save_data(data: pd.DataFrame, save_path: Path) -> None:
+    data.to_csv(save_path, index=False)
     
-    return preprocessor.transform(df)
 
+def make_X_and_y(data:pd.DataFrame, target_column: str):
+    X = data.drop(columns=[target_column])
+    y = data[target_column]
+    return X, y
 
-def save_data(data:pd.DataFrame, save_path:Path)->None:
-    data.to_csv(save_path , index = False)
-
-def save_transformer(transformer, save_dir:Path, transformer_name:str):
-
-    save_path = save_dir / transformer_name
-
-    try:
-        joblib.dump(value = transformer , filename= save_path)
-        logger.info(f"{transformer_name} saved to location :{save_dir}")
-
-    except Exception as e:
-        logger.error(f"Error while saving {transformer_name} transformer.")
-
-
-
-def join_X_and_y(X:pd.DataFrame , y:pd.Series):
-
-    join_data = X.join(y , how = 'inner')
-
-    return join_data
-
-
-
+def join_X_and_y(X: pd.DataFrame, y: pd.Series):
+    # join based on indexes
+    joined_df = X.join(y,how='inner')
+    return joined_df
+    
+    
 if __name__ == "__main__":
-
+    # paths
+    # root path
+    # root path
     root_path = Path(__file__).parent.parent.parent
-
-    train_data_path = root_path/ "data" / "interim" / "train.csv"
+    # data load path
+    train_data_path = root_path / "data" / "interim" / "train.csv"
     test_data_path = root_path / "data" / "interim" / "test.csv"
-
+    # save data directory
     save_data_dir = root_path / "data" / "processed"
-
-    save_data_dir.mkdir(parents=True ,exist_ok= True)
-
-    train_processed_file_name = "train_processed.csv"
-    test_processed_file_name = "test_processed.csv"
-
-    save_train_processed_path = save_data_dir / train_processed_file_name
-    save_test_processed_path = save_data_dir / test_processed_file_name
-
-
-
+    # make dir if not preseny
+    save_data_dir.mkdir(exist_ok=True,parents=True)
+    # train and test data save paths
+    # filenames
+    train_trans_filename = "train_trans.csv"
+    test_trans_filename = "test_trans.csv"
+    # save path for train and test
+    save_train_trans_path = save_data_dir / train_trans_filename
+    save_test_trans_path = save_data_dir / test_trans_filename
+    
+    # preprocessor
     preprocessor = ColumnTransformer(transformers=[
             ("scale", MinMaxScaler(), num_cols),
             ("nominal_encode", OneHotEncoder(drop="first",
@@ -157,45 +141,43 @@ if __name__ == "__main__":
                                     n_jobs=-1,
                                     force_int_remainder_cols=False,
                                     verbose_feature_names_out=False)
-
-
+    
+    
+    # load the train and test data with missing values dropped
     train_df = drop_missing_values(load_data(data_path=train_data_path))
     logger.info("Train data loaded successfully")
     test_df = drop_missing_values(load_data(data_path=test_data_path))
     logger.info("Test data loaded successfully")
-
-
+    
+    # split the train and test data
     X_train, y_train = make_X_and_y(data=train_df,target_column=target_col)
     X_test, y_test = make_X_and_y(data=test_df, target_column=target_col)
     logger.info("Data splitting completed")
-
-
-    train_preprocessor(preprocessor=preprocessor, df=X_train)
+    
+    # fit the preprocessor on X_train
+    train_preprocessor(preprocessor=preprocessor, data=X_train)
     logger.info("Preprocessor is trained")
-
-
-    X_train_trans =  perform_transformations(preprocessor=preprocessor, df=X_train)
+    
+    # transform the data
+    X_train_trans =  perform_transformations(preprocessor=preprocessor, data=X_train)
     logger.info("Train data is transformed")
-    X_test_trans = perform_transformations(preprocessor=preprocessor, df=X_test)
+    X_test_trans = perform_transformations(preprocessor=preprocessor, data=X_test)
     logger.info("Test data is transformed")
-
-
-
+    
+    # join back X and y
     train_trans_df = join_X_and_y(X_train_trans, y_train)
     test_trans_df = join_X_and_y(X_test_trans, y_test)
     logger.info("Datasets joined")
-
-
+    
+    # save the transformed data
     data_subsets = [train_trans_df, test_trans_df]
-    data_paths = [save_train_processed_path,save_test_processed_path]
-    filename_list = [train_processed_file_name, test_processed_file_name]
-
-
+    data_paths = [save_train_trans_path,save_test_trans_path]
+    filename_list = [train_trans_filename, test_trans_filename]
     for filename , path, data in zip(filename_list, data_paths, data_subsets):
         save_data(data=data, save_path=path)
-        logger.info(f"{filename.replace(".csv","")} data saved to location")
-
-
+        
+    # save the preprocessor to location
+    # transformer name
     transformer_filename = "preprocessor.joblib"
     # directory to save transformers
     transformer_save_dir = root_path / "models"
